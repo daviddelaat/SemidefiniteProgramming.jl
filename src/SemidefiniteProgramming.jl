4e623f65887f119ead3c409e8496d8b7bc3c627a
David de Laat <mail@daviddelaat.nl>
2013-04-05 16:40:47 +0200

    Reworked SparseSDP type

--- src/SemidefiniteProgramming.jl
+++ src/SemidefiniteProgramming.jl
@@ -3,87 +3,164 @@ module SemidefiniteProgramming
 import 
     Base.print, 
     Base.show
-
+    
 export 
-    Rel, 
-    SparseSDP, 
+    SparseSDP,
+    blocksizes,
+    setrel!,
+    getrel,
+    setrhs!,
+    getrhs,
+    nrels,
+    rhs,
+    writesdpasparse,
     SDPSolver,
     SDPA,
     SDPAQD,
     SDPAGMP,
     CSDP,
     solve
+   
+    
+    
+    
 
-
     
     
     
-# Sparse SDPA format
-    
-immutable Rel{T<:Number}
-    nr::Int
-    blocknr::Int
-    i::Int
-    j::Int
-    val::T
-end
-
 type SparseSDP{T<:Number}
-    blocksizes::Vector{Int}
-    rhs::Vector{T}
-    rels::Vector{Rel{T}}
+    rhs::Dict{Int, T}
+    rels::Dict{(Int, Int, Int, Int), T}
 end
 
-function SparseSDP{T<:Number}(rhs::Vector{T}, rels::Vector{Rel{T}})
-    m = 0
-    for rel in rels
-        if rel.blocknr > m
-            m = rel.blocknr
-        end
+SparseSDP{T<:Number}() = SparseSDP(Dict{Int, T}(), Dict{(Int, Int, Int, Int), T}())
+SparseSDP() = SparseSDP(Dict{Int, Float64}(), Dict{(Int, Int, Int, Int), Float64}())
+
+setrhs!{T<:Number}(sdp::SparseSDP{T}, ri::Int, v::T) = sdp.rhs[ri] = v       
+getrhs(sdp::SparseSDP, ri::Int) = sdp.rhs[ri]
+
+function setrel!{T<:Number}(sdp::SparseSDP{T}, ri::Int, bi::Int, i::Int, j::Int, v::T) 
+    if i <= j
+        sdp.rels[(ri, bi, i, j)] = v
+    else
+        sdp.rels[(ri, bi, j, i)] = v
     end
-    blocksizes = zeros(Int, m)
-    for rel in rels
-        m = max(rel.i, rel.j)
-        if m > blocksizes[rel.blocknr]
-            blocksizes[rel.blocknr] = m
-        end
-    end
-    SparseSDP(blocksizes, rhs, rels)
 end
 
-function print(io::IO, sdp::SparseSDP)
-    println(io, length(sdp.rhs))
-    println(io, length(sdp.blocksizes))
+setrel!{T<:Number}(sdp::SparseSDP{T}, ri::Int, i::Int, j::Int, v::T) = setrel!(sdp, ri, 1, i, j, v)
+
+function getrel(sdp::SparseSDP, ri::Int, bi::Int, i::Int, j::Int)
+    if i <= j
+        sdp.rels[(ri, bi, i, j)]
+    else
+        sdp.rels[(ri, bi, j, i)]
+    end
+end
+
+getrel(sdp::SparseSDP, ri::Int, i::Int, j::Int) = getrel(sdp, ri, 1, i, j)
+
+function setrel!{T<:Number}(sdp::SparseSDP{T}, ri::Int, bi::Int, m::Matrix{T}) 
+    for i = 1:dims(m)[1]
+        for j = 1:i
+            if m[i, j] != zero(T)
+                setrel!(sdp, ri, bi, i, j) = m[i, j]
+            end
+        end
+    end 
+end
+
+setrel!{T<:Number}(sdp::SparseSDP{T}, ri::Int, m::Matrix{T}) = setrel!(sdp, ri, 1, m)
+
+function blocksizes(sdp::SparseSDP)
+    bsizes = Dict{Int, Int}()
+    mbi = 0
+    for (ri, bi, i, j) in keys(sdp.rels)
+        if bi > mbi
+            mbi = bi
+        end
+        m = max(i, j)
+        if m > get(bsizes, bi, 0)
+            bsizes[bi] = m
+        end
+    end
+    bsizeslist = zeros(Int, mbi)
+    for (bi, bsize) in bsizes
+        bsizeslist[bi] = bsize
+    end
+    bsizeslist
+end
+
+function nrels(sdp::SparseSDP)
+    c = -1
+    for (ri, bi, i, j) in keys(sdp.rels)
+        if ri > c
+            c = ri
+        end
+    end
+    for ri in keys(sdp.rhs)
+        if ri > c
+            c = ri
+        end
+    end
+    c    
+end
+
+function rhs(sdp::SparseSDP)
+    l = zeros(nrels(sdp))
+    for (ri, val) in sdp.rhs
+        l[ri] = val 
+    end
+    l
+end
+
+function writesdpasparse(io::IO, sdp::SparseSDP)
+    bsizes = blocksizes(sdp)    
+
+    println(io, nrels(sdp))
+    println(io, length(bsizes))
     
-    for b in sdp.blocksizes[1:end-1]
+    for b in bsizes[1:end-1]
         print(io, b)
         print(io, " ")
     end
-    println(io, sdp.blocksizes[end])
+    if length(bsizes) > 0
+        println(io, bsizes[end])
+    end
     
-    for r in sdp.rhs[1:end-1]
+    rhslist = rhs(sdp)
+    for r in rhslist[1:end-1]
         print(io, r)
         print(io, " ")
     end
-    println(io, sdp.rhs[end])
+    if length(rhslist) > 0
+        println(io, rhslist[end])
+    end
     
-    for rel in sdp.rels
-        print(io, rel.nr)
+    for ((ri, bi, i, j), val) in sdp.rels
+        print(io, ri)
         print(io, " ")
-        print(io, rel.blocknr)
+        print(io, bi)
         print(io, " ")
-        print(io, rel.i)
+        print(io, i)
         print(io, " ")
-        print(io, rel.j)
+        print(io, j)
         print(io, " ")
-        print(io, rel.val)
+        print(io, val)
         println(io)  
     end
 end
 
-function show(io::IO, sdp::SparseSDP)
-    println("$(length(sdp.blocksizes))-block sparse semidefinite program:")
-    print(io, sdp)
+function show{T<:Number}(io::IO, sdp::SparseSDP{T})
+    bsizes = blocksizes(sdp)
+    nr = nrels(sdp)
+    
+    print(io, "SparseSDP with blocksizes ")
+    println(io, bsizes)
+    
+    for i in 0:nr
+        println(io, "Relation $i with right hand side $(get(sdp.rhs, i, zero(T))): ")
+        
+    end
 end
 
 
@@ -91,7 +168,6 @@ end
 
 
 
-# Solvers
 
 abstract SDPSolver
 
@@ -123,9 +199,9 @@ immutable CSDP <: SDPSolver
 end
 CSDP() = CSDP("csdp")
 
-function solve{T<:SDPAGEN}(sdp::SparseSDP, solver::T)
+function solve(sdp::SparseSDP, solver::SDPAGEN)
     datafname, dataio = mktemp()
-    print(dataio, sdp)
+    writesdpasparse(dataio, sdp)
     flush(dataio)
     for l in eachline(`$(solver.executable) -ds $datafname -o /dev/null`)
         if beginswith(l, "objValPrimal = ")
@@ -134,12 +210,12 @@ function solve{T<:SDPAGEN}(sdp::SparseSDP, solver::T)
         end      
     end
     close(dataio)
-    return nothing
+    nothing
 end
 
 function solve(sdp::SparseSDP, solver::CSDP)
     datafname, dataio = mktemp()
-    print(dataio, sdp)
+    writesdpasparse(dataio, sdp)
     flush(dataio)    
     for l in eachline(`$(solver.executable) $datafname /dev/null`)
         if beginswith(l, "Primal objective value: ")
@@ -148,7 +224,7 @@ function solve(sdp::SparseSDP, solver::CSDP)
         end
     end
     close(dataio)
-    return nothing
+    nothing
 end
 
 end
